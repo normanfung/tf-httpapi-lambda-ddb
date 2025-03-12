47fbf144eb69611ead64ad809f0eeb2d12e39acf
normanfung <56857601+normanfung@users.noreply.github.com>
2025-03-12 21:38:44 +0800

    Added Log Groups and Cloudwatch Alarm

--- apigateway.tf
+++ apigateway.tf
@@ -9,30 +9,30 @@ resource "aws_apigatewayv2_stage" "default" {
   name        = "$default"
   auto_deploy = true
 
-  # access_log_settings {
-  #   destination_arn = aws_cloudwatch_log_group.api_access_logs.arn
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.api_access_logs.arn
 
-  #   format = jsonencode({
-  #     requestId               = "$context.requestId"
-  #     sourceIp                = "$context.identity.sourceIp"
-  #     requestTime             = "$context.requestTime"
-  #     protocol                = "$context.protocol"
-  #     httpMethod              = "$context.httpMethod"
-  #     resourcePath            = "$context.resourcePath"
-  #     routeKey                = "$context.routeKey"
-  #     status                  = "$context.status"
-  #     responseLength          = "$context.responseLength"
-  #     integrationErrorMessage = "$context.integrationErrorMessage"
-  #     }
-  #   )
-  # }
-  # depends_on = [aws_cloudwatch_log_group.api_access_logs]
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+  depends_on = [aws_cloudwatch_log_group.api_access_logs]
 }
 
-# resource "aws_cloudwatch_log_group" "api_access_logs" {
-#   name = "/aws/api_gw/${aws_apigatewayv2_api.http_api.name}"
-#   retention_in_days = 7
-# }
+resource "aws_cloudwatch_log_group" "api_access_logs" {
+  name              = "/aws/api_gw/${aws_apigatewayv2_api.http_api.name}"
+  retention_in_days = 7
+}
 
 resource "aws_apigatewayv2_integration" "apigw_lambda" {
   api_id = aws_apigatewayv2_api.http_api.id
--- lambda.tf
+++ lambda.tf
@@ -19,13 +19,26 @@ resource "aws_lambda_function" "http_api_lambda" {
     }
   }
 
-  # depends_on = [ aws_cloudwatch_log_group.http_api ]
+  depends_on = [aws_cloudwatch_log_group.http_api]
 }
 
-# resource "aws_cloudwatch_log_group" "http_api" {
-#   name              = "/aws/lambda/${local.name_prefix}-topmovies-api"
-#   retention_in_days = 7
-# }
+resource "aws_cloudwatch_log_group" "http_api" {
+  name              = "/aws/lambda/${local.name_prefix}-topmovies-api"
+  retention_in_days = 7
+}
+
+resource "aws_cloudwatch_log_metric_filter" "yada" {
+  name           = "info-count"
+  pattern        = "[INFO]"
+  log_group_name = aws_cloudwatch_log_group.http_api.name
+
+  metric_transformation {
+    name      = "info-count"
+    namespace = "/moviedb-api/norman-ce9"
+    value     = "1"
+    unit      = "None"
+  }
+}
 
 resource "aws_iam_role" "lambda_exec" {
   name = "${local.name_prefix}-topmovies-api-executionrole"
